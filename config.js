cf369e11aa3da7b4dc11b8cd9e2b5463367cd6da
sadeeshkumar-gopalan <mg.sadeeshkumar@gmail.com>
2026-01-14 21:21:18 +0530

    Update config.js

--- config.js
+++ config.js
@@ -3,10 +3,11 @@
 
 // Production backend URL (Hugging Face Space)
 // Note: Hugging Face Spaces use the .hf.space subdomain format
-window.API_BASE_URL = 'https://nithiyanandham15-blog-website-backend.hf.space';
+window.API_BASE_URL = 'https://sadeeshkumargopalan-blog-website-backend.hf.space/;
 
 // For local development, uncomment and use:
 // window.API_BASE_URL = 'http://localhost:8000';
 
 // To use this config, include this file before other scripts:
 // <script src="config.js"></script>
+
